2d0d247bc8602b2c6475ed8868bc5ccc6394fcc0
Beth Josey <bethjosey@outlook.com>
2018-03-21 15:35:52 -0400

    Converting Arduino to Mac

---
+++ arduino_mac/app/js/main.js
@@ -0,0 +1,234 @@
+var Readable = require('stream').Readable  
+var util = require('util')  
+var five = require('johnny-five')
+
+util.inherits(MyStream, Readable)  
+function MyStream(opt) {  
+  Readable.call(this, opt)
+}
+MyStream.prototype._read = function() {};  
+// hook in our stream
+process.__defineGetter__('stdin', function() {  
+  if (process.__stdin) return process.__stdin
+  process.__stdin = new MyStream()
+  return process.__stdin
+})
+
+var board = new five.Board();
+
+board.on("ready", function() {
+
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
+var winner = document.querySelectorAll('.winner');
+
+function _(x){
+	return document.getElementById(x);
+}
+
+function renderQuestion(){
+	test = _("test");
+	for(var i = 0; i < winner.length; i++) {
+		winner[i].style.backgroundColor = "transparent";
+    }
+	if(pos >= questions.length){
+		test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+		_("test_status").innerHTML = "WINNER!";
+		pos = 0;
+		correct = 0;
+		stepper.cw().step(5000, function() {
+    		console.log("done");
+   		});
+   		stepper2.cw().step(5000, function() {
+    		console.log("done");
+   		});
+		return false;
+	}
+	_("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+	var cat = Math.floor(Math.random()*3);
+	question = questions[pos][cat][0];
+	chA = questions[pos][cat][1];
+	chB = questions[pos][cat][2];
+	chC = questions[pos][cat][3];
+	chD = questions[pos][cat][4];
+	document.querySelector("#question").innerHTML = question;
+	winner[0].innerHTML = chA;
+	winner[1].innerHTML = chB;
+	winner[2].innerHTML = chC;
+	winner[3].innerHTML = chD;
+	test.innerHTML = "<h3>"+question+"</h3>";
+	test.innerHTML += "<input id='one0' type='radio' name='choices' value='A'> "+chA+"<br>";
+	test.innerHTML += "<input id='one1' type='radio' name='choices' value='B'> "+chB+"<br>";
+	test.innerHTML += "<input id='one2' type='radio' name='choices' value='C'> "+chC+"<br>";
+	test.innerHTML += "<input id='one3' type='radio' name='choices' value='D'> "+chD+"<br><br>";
+	// test.innerHTML += "<button id='tested'>Submit Answer</button>";
+
+
+	setTimeout(function() {
+			console.log(cat);
+			// choices = document.getElementsByName("choices");
+			var dog1 = document.querySelector("#one0");
+			var dog2 = document.querySelector("#one1");
+			var dog3 = document.querySelector("#one2");
+			var dog4 = document.querySelector("#one3");
+			// for(var i = 0; i<choices.length; i++){
+			if(dog1.checked){
+				choice = dog1.value;
+				console.log('one');
+				var dog = dog1.id.charAt(3);
+			}else if (dog2.checked) {
+				choice = dog2.value;
+				console.log('two');
+				var dog = dog2.id.charAt(3);
+			}else if (dog3.checked) {
+				choice = dog3.value;
+				console.log('three');
+				var dog = dog3.id.charAt(3);
+			}else if (dog4.checked) {
+				choice = dog4.value;
+				console.log('four');
+				var dog = dog4.id.charAt(3);
+			}else{
+				pos = 0;
+				correct = 0;
+				console.log
+				_("showRight").innerHTML = "no answer";
+				setTimeout(renderQuestion, 5000);
+				return;
+			}
+			// 	}else{
+			// 		_("showRight").innerHTML = "no answer";
+			// 		pos = 0;
+			// 		correct = 0;
+			// 		setTimeout(renderQuestion, 5000);
+			// 		return;
+			// 	}
+			// }
+			if(choice == questions[pos][cat][5]){
+				correct++;
+				for(var i = 0; i < winner.length; i++) {
+    			winner[i].style.backgroundColor = "transparent";
+    			}	
+    			if(dog) {	
+					winner[dog].style.backgroundColor = "green";
+					_("showRight").innerHTML = "right";
+				}
+				pos++;
+			}else {
+				_("showRight").innerHTML = "wrong";
+				pos = 0;
+				correct = 0;
+				renderQuestion();
+				_("showRight").innerHTML = "";
+				return false;
+			}
+			setTimeout(renderQuestion, 1000);
+	}, 10000);
+}
+
+// function checkAnswer(random){
+// 	console.log(random);
+// 	choices = document.getElementsByName("choices");
+// 	for(var i=0; i<choices.length; i++){
+// 		if(choices[i].checked){
+// 			choice = choices[i].value;
+// 		}
+// 	}
+// 	if(choice == questions[pos][1][5]){
+// 		correct++;
+// 		_("showRight").innerHTML = "right";
+// 	}else {
+// 		_("showRight").innerHTML = "wrong";
+// 		pos = 0;
+// 		correct = 0;
+// 		renderQuestion();
+// 		_("showRight").innerHTML = "";
+// 		return false;
+// 	}
+// 	pos++;
+// 	renderQuestion();
+// }
+
+
+// window.addEventListener("load", renderQuestion, false);
+renderQuestion();
+
+var buttonred = new five.Button(2);
+
+var buttonblue = new five.Button(3);
+
+var buttonyellow = new five.Button(4);
+
+var buttongreen = new five.Button(5);
+
+
+
+var stepper = new five.Stepper({
+    type: five.Stepper.TYPE.FOUR_WIRE,
+    stepsPerRev: 150,
+    pins: [6,7,8,9]
+  });
+
+var stepper2 = new five.Stepper({
+    type: five.Stepper.TYPE.FOUR_WIRE,
+    stepsPerRev: 150,
+    pins: [10,11,12,13]
+  });
+
+// function moveCC() {
+//   stepper.cw().step(5000, function() {
+//     console.log("done");
+//   });
+// }
+
+// button.on("press", function() {
+//  info.innerHTML += "button pushed. ";
+
+  buttonred.on("hold", function() {
+    // buttonInfo.innerHTML += "Red button pushed";
+    //console.log('red');
+    // stepper.cw().step(5000, function() {
+    // console.log("done");
+    // });
+    for(var i = 0; i < winner.length; i++) {
+    	winner[i].style.backgroundColor = "transparent";
+    }
+    winner[0].style.backgroundColor = "yellow";
+    var one = document.querySelector("#one0");
+    one.checked = true;
+  });
+
+  buttonblue.on("hold", function() {
+    // buttonInfo.innerHTML += "Blue button pushed";
+    // //console.log('blue');
+    for(var i = 0; i < winner.length; i++) {
+    	winner[i].style.backgroundColor = "transparent";
+    }
+    winner[1].style.backgroundColor = "yellow";
+    var two = document.querySelector("#one1");
+    two.checked = true;
+  });
+
+  buttonyellow.on("hold", function() {
+    // buttonInfo.innerHTML += "Yellow button pushed";
+    // //console.log('yellow');
+    for(var i = 0; i < winner.length; i++) {
+    	winner[i].style.backgroundColor = "transparent";
+    }
+    winner[2].style.backgroundColor = "yellow";
+    var three = document.querySelector("#one2");
+    three.checked = true;
+  });
+
+  buttongreen.on("hold", function() {
+    // buttonInfo.innerHTML += "Green button pushed";
+    // //console.log('green');
+    for(var i = 0; i < winner.length; i++) {
+    	winner[i].style.backgroundColor = "transparent";
+    }
+    winner[3].style.backgroundColor = "yellow";
+    var four = document.querySelector("#one3");
+    four.checked = true;
+  });
+
+});
+
---
+++ arduino_mac/app/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    [
+    	["Choose the correct HTML tag for the largest heading?", "&lt;heading&gt;", "&lt;h6&gt;", "&lt;head&gt;", "&lt;h1&gt;", "D"],
+    	["What does CSS stand for?", "Cascading Style Sheet", "Complete Style Syntax", "Complete Sound Sheet", "Cats standing still", "A"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is an advantage of having external CSS?", "It is faster.", "You can edit one file to edit the base of every page which uses it.", "It is less confusing.", "There are no advantages.", "B"],
+    	["What are Sass, Stylus and LESS?", "Types of CSS", "Repsonsive CSS grid systems", "CSS pre-processors", "CSS frameworks", "C"],
+    	["What is NOT an HTML5 element?", "section", "canvas", "audio", "block", "D"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ],
+    [
+    	["What is one of the reasons Java is different to Javascript?", "Java is compiled before it runs and isn't limited to being on a web browser", "Javascript is compiled before it runs and isn't limited to being on a web browser", "Javascript is barely used anymore since Java came out", "Java is just short for javascript", "B"],
+    	["What is the newest version of HTML being developed?", "HTML4", "HTML3", "HTML5", "There is only type of HTML", "C"],
+    	["What does HTML stand for?", "Hyper type marked language", "Hyper text markup language", "Hyped terrain mock language", "Hyper typeface main loop", "B"]
+    ]
+];
